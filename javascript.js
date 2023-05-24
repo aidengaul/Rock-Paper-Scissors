8bb386d57141e2a10f4e3eed0619578540b5021c
Aiden Gaul <100671205+aidengaul@users.noreply.github.com>
2023-05-24 02:29:08 -0400

    Adds game logic and score keeping

--- javascript.js
+++ javascript.js
@@ -8,12 +8,41 @@ function getComputerChoice() {
         return "scissors";
 }
 
+function updateScores(roundResult) {
+    const playerScore = document.querySelector('#player-score');
+    const computerScore = document.querySelector('#computer-score');
+
+    if (roundResult) {
+        playerScore.textContent = parseInt(playerScore.textContent) + 1;
+    }
+    else {
+        computerScore.textContent = parseInt(computerScore.textContent) + 1;
+    }
+}
+
 function playRound(e) {
     const playerSelection = e.target.id;
     const computerSelection = getComputerChoice();
-    
+
     console.log(playerSelection);
     console.log(computerSelection);
+
+    const messageField = document.querySelector('#message');
+    let playerWon = false;
+
+    if (computerSelection === playerSelection) {
+        messageField.textContent = "It's a tie!";
+        return;
+    }
+    else if (computerSelection === "rock" && playerSelection === "scissors" || computerSelection === "paper" && playerSelection === "rock" || computerSelection === "scissors" && playerSelection === "paper") {
+        messageField.textContent = "You lost this round!";
+    }
+    else {
+        messageField.textContent = "You won this round!";
+        playerWon = true;
+    }
+
+    updateScores(playerWon);
 }
 
 const buttons = document.querySelectorAll('button');
